8a184db18898c17bc81e64fdd2a3081e970aec6e
loveu9911 <loveu9911@naver.com>
2026-03-19 17:26:36 +0900

    fix index

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
-console.log("Jenkins + Node 실행 성공");
+import http from 'http';
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Jenkins CI/CD 🚀');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
